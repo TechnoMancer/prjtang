8bf2da59420afcbd1a2b932a3205bd97d69a7022
Paul Davey <plmdvy@gmail.com>
2019-12-07 01:53:09 +1300

    Import and update html generation to match trellis
    Use the trellis html docs creation scripts these will generate bit docs
    for all tiles as well as generate the FPGA docs.  This requires a
    setting "fuzz" to be present in the device description in devices.json

--- tools/html_all.py
+++ tools/html_all.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 """
-For each family and device, create tilegrid html
+Project Tang Master HTML Generation Script
+
+Usage:
+html_all.py <output_folder>
 """
 
-import os, time
+import os, sys, time
 from os import path
 from string import Template
+import argparse
 import database
-import json
 import html_tilegrid
-import shutil
+import html_bits
+import pytang
 
 tang_docs_index = """
 <html>
@@ -35,28 +39,79 @@ $docs_toc
 </html>
 """
 
-def main():
-	devices = database.get_devices()
-	shutil.rmtree("work_html", ignore_errors=True)
-	os.mkdir("work_html")
-	commit_hash = database.get_db_commit()
-	build_dt = time.strftime('%Y-%m-%d %H:%M:%S')
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('fld', type=str,
+                    help="output folder")
 
-	docs_toc = ""
-	for family in sorted(devices["families"].keys()):
-		docs_toc += "<h3>{} Family</h3>".format(family.upper())
-		docs_toc += "<h4>Bitstream Documentation</h4>"
-		docs_toc += "<ul>"
-		for device in sorted(devices["families"][family]["devices"].keys()):
-			docs_toc += '<li><a href="{}">{} Documentation</a></li>'.format('{}.html'.format(device),device.upper())
-			html_tilegrid.main(["html_tilegrid", family,  device, path.join("work_html",device + ".html")])
-		docs_toc += "</ul>"
-	index_html = Template(tang_docs_index).substitute(
-		datetime=build_dt,
-		commit=commit_hash,
-		docs_toc=docs_toc
-	)
-	with open(path.join("work_html", "index.html"), 'w') as f:
-		f.write(index_html)
+
+def generate_device_docs(family, device, folder):
+    html_tilegrid.main(["html_tilegrid", family, device, path.join(folder, "index.html")])
+
+
+def generate_tile_docs(family, device, tile, folder):
+    html_bits.main(["html_bits", family, device, tile, path.join(folder, "{}.html".format(tile))])
+
+
+def get_device_tiles(family, devices):
+    all_tiles = set()
+    fd_tiles = {}
+    for dev, devdata in sorted(devices.items()):
+        if "fuzz" in devdata and devdata["fuzz"]:
+            fd_tiles[family, dev] = []
+            c = pytang.Chip(dev)
+            for tile in c.get_all_tiles():
+                tt = tile.info.type
+                if tt not in all_tiles:
+                    all_tiles.add(tt)
+                    fd_tiles[family, dev].append(tt)
+    return fd_tiles
+
+
+def main(argv):
+    args = parser.parse_args(argv[1:])
+    if not path.exists(args.fld):
+        os.mkdir(args.fld)
+    commit_hash = database.get_db_commit()
+    build_dt = time.strftime('%Y-%m-%d %H:%M:%S')
+    docs_toc = ""
+    pytang.load_database(database.get_db_root())
+    for fam, fam_data in sorted(database.get_devices()["families"].items()):
+        fdir = path.join(args.fld, fam)
+        if not path.exists(fdir):
+            os.mkdir(fdir)
+        thdir = path.join(fdir, "tilehtml")
+        if not path.exists(thdir):
+            os.mkdir(thdir)
+        docs_toc += "<h3>{} Family</h3>".format(fam)
+        docs_toc += "<h4>Bitstream Documentation</h4>"
+        docs_toc += "<ul>"
+        tiles = get_device_tiles(fam, fam_data["devices"])
+        for dev, devdata in sorted(fam_data["devices"].items()):
+            if "fuzz" in devdata and devdata["fuzz"]:
+                ddir = path.join(fdir, dev)
+                if not path.exists(ddir):
+                    os.mkdir(ddir)
+                print("********* Generating documentation for device {}".format(dev))
+                generate_device_docs(fam, dev, ddir)
+                if (fam, dev) in tiles:
+                    for tile in tiles[fam, dev]:
+                        print("*** Generating documentation for tile {}".format(tile))
+                        generate_tile_docs(fam, dev, tile, thdir)
+                docs_toc += '<li><a href="{}">{} Documentation</a></li>'.format(
+                    '{}/{}/index.html'.format(fam, dev),
+                    dev
+                )
+
+        docs_toc += "</ul>"
+
+    index_html = Template(tang_docs_index).substitute(
+        datetime=build_dt,
+        commit=commit_hash,
+        docs_toc=docs_toc
+    )
+    with open(path.join(args.fld, "index.html"), 'w') as f:
+        f.write(index_html)
+
+
 if __name__ == "__main__":
-	main()
+    main(sys.argv)
---
+++ tools/html_bits.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python3
+
+"""Generate a HTML representation of the tile bit database"""
+
+import pytang
+import database
+import argparse
+import sys
+import re
+
+
+def find_bits(db):
+    cwords = db.get_settings_words()
+    for cword in cwords:
+        wd = db.get_data_for_setword(cword)
+        for i in range(len(wd.bits)):
+            for bit in wd.bits[i].bits:
+                bitmap[bit.frame, bit.bit] = "word_" + str(cword)
+                if (bit.frame, bit.bit) not in labels:
+                    labels[bit.frame, bit.bit] = set()
+                labels[bit.frame, bit.bit].add("{}[{}]".format(cword, i))
+    cenums = db.get_settings_enums()
+    for cenum in cenums:
+        ed = db.get_data_for_enum(cenum)
+        for opt in ed.get_options():
+            for bit in ed.options[opt].bits:
+                bitmap[bit.frame, bit.bit] = "enum_" + str(cenum)
+                if (bit.frame, bit.bit) not in labels:
+                    labels[bit.frame, bit.bit] = set()
+                labels[bit.frame, bit.bit].add(cenum)
+    sinks = db.get_sinks()
+    for sink in sinks:
+        mux = db.get_mux_data_for_sink(sink)
+        for src in mux.get_sources():
+            for bit in mux.arcs[src].bits.bits:
+                bitmap[bit.frame, bit.bit] = "mux_" + str(sink)
+                if (bit.frame, bit.bit) not in labels:
+                    labels[bit.frame, bit.bit] = set()
+                labels[bit.frame, bit.bit].add(sink)
+
+
+def mux_html(mux, f):
+    bitset = set()
+    for src in mux.get_sources():
+        for bit in mux.arcs[src].bits.bits:
+            bitset.add((bit.frame, bit.bit))
+
+    bitlist = list(sorted(bitset))
+    print('<h3 id="mux_{}">Mux driving <span class="mux_sink">{}</span></h3>'.format(mux.sink, mux.sink), file=f)
+    print('<table class="mux"><tr><th>Source</th>', file=f)
+    for bit in bitlist:
+        print('<th style="padding-left: 10px; padding-right: 10px">F{}B{}</th>'.format(bit[0], bit[1]), file=f)
+    print('</tr>', file=f)
+    truthtable = []
+    for src in mux.get_sources():
+        ttrow = []
+        for blb in bitlist:
+            found = False
+            for ab in mux.arcs[src].bits.bits:
+                if ab.frame == blb[0] and ab.bit == blb[1]:
+                    ttrow.append('0' if ab.inv else '1')
+                    found = True
+                    break
+            if not found:
+                ttrow.append("-")
+        truthtable.append((mux.arcs[src], ttrow))
+    trstyle = ""
+    for (arc, ttrow) in sorted(truthtable, key=lambda x: "".join(reversed(x[1])).replace("-", "0")):
+        trstyle = " bgcolor=\"#dddddd\"" if trstyle == "" else ""
+        print('<tr {}><td>{}</td>'.format(trstyle, arc.source), file=f)
+        for bit in ttrow:
+            print('<td style="text-align: center">{}</td>'.format(bit), file=f)
+        print('</td>', file=f)
+
+    print('</table>', file=f)
+
+
+def setword_html(word, f):
+    print('<h3 id="word_{}">Configuration {} {}</h3>'.format(word.name, "bit" if len(word.bits) == 1 else "word",
+                                                             word.name), file=f)
+    defval_bits = []
+    for i in range(len(word.defval)):
+        if len(word.bits[i].bits) == 0:
+            defval_bits.append("X")
+        else:
+            defval_bits.append("1" if word.defval[i] else "0")
+    defval = "{}'b{}".format(len(defval_bits), "".join(reversed(defval_bits)))
+    print('<p>Default value: {}</p>'.format(defval), file=f)
+    print('<table class="setword">', file=f)
+    trstyle = ""
+    for idx in range(len(word.bits)):
+        cbits = " ".join(["{}F{}B{}".format("!" if b.inv else "", b.frame, b.bit) for b in word.bits[idx].bits])
+        trstyle = " bgcolor=\"#dddddd\"" if trstyle == "" else ""
+        print(
+            '<tr {}><td style="padding-left: 10px; padding-right: 10px">{}[{}]</td><td style="padding-left: 10px; padding-right: 10px">{}</td></tr>'.format(
+                trstyle, word.name, idx, cbits), file=f)
+    print('</table>', file=f)
+
+
+def nice_sort(x):
+    if x == "NONE":
+        return -1, ""
+    else:
+        n = re.findall('\d+', x)
+        if len(n) > 0:
+            return int("".join(n)), x
+        else:
+            return 0, x
+
+
+def setenum_html(enum, f):
+    print('<h3 id="enum_{}">Configuration Setting {}</h3>'.format(enum.name, enum.name), file=f)
+    if enum.defval != "":
+        print('<p>Default value: {}</p>'.format(enum.defval), file=f)
+    bitset = set()
+    for opt in enum.get_options():
+        for bit in enum.options[opt].bits:
+            bitset.add((bit.frame, bit.bit))
+
+    bitlist = list(sorted(bitset))
+    print('<table class="setenum"><tr><th>Value</th>', file=f)
+    for bit in bitlist:
+        print('<th style="padding-left: 10px; padding-right: 10px">F{}B{}</th>'.format(bit[0], bit[1]), file=f)
+    print("</tr>", file=f)
+    truthtable = []
+    for opt in enum.get_options():
+        ttrow = []
+        for blb in bitlist:
+            found = False
+            for ab in enum.options[opt].bits:
+                if ab.frame == blb[0] and ab.bit == blb[1]:
+                    ttrow.append('0' if ab.inv else '1')
+                    found = True
+                    break
+            if not found:
+                ttrow.append("-")
+        truthtable.append((opt, ttrow))
+    trstyle = ""
+    tt_sorted = sorted(truthtable, key=lambda x: nice_sort(x[0]))
+    for (opt, ttrow) in tt_sorted:
+        trstyle = " bgcolor=\"#dddddd\"" if trstyle == "" else ""
+        print('<tr {}><td>{}</td>'.format(trstyle, opt), file=f)
+        for bit in ttrow:
+            print('<td style="text-align: center">{}</td>'.format(bit), file=f)
+        print('</td>', file=f)
+    print('</table>', file=f)
+
+
+def muxes_html(db, f):
+    sinks = db.get_sinks()
+    for sink in sorted(sinks):
+        mux = db.get_mux_data_for_sink(sink)
+        mux_html(mux, f)
+
+
+def setwords_html(db, f):
+    words = db.get_settings_words()
+    for name in sorted(words):
+        word = db.get_data_for_setword(name)
+        setword_html(word, f)
+
+
+def setenums_html(db, f):
+    enums = db.get_settings_enums()
+    for name in sorted(enums):
+        enum = db.get_data_for_enum(name)
+        setenum_html(enum, f)
+
+
+def fixed_conns_html(db, f):
+    conns = db.get_fixed_conns()
+    if len(conns) > 0:
+        print("<h3>Fixed Connections</h3>", file=f)
+        print('<table class="fconn" style="border-spacing:0"><tr><th>Source</th><th></th><th>Sink</th></tr>', file=f)
+        trstyle = ""
+        sorted_conns = sorted(conns, key=lambda x: re.sub(r"^([NESW]\d+)+_", "", x.sink))
+        for conn in sorted_conns:
+            trstyle = " bgcolor=\"#dddddd\"" if trstyle == "" else ""
+            print(
+                """<tr {}><td style="padding-left: 10px; padding-right: 10px; margin-left: 0px;">{}</td><td>&rarr;</td>
+                <td style="padding-left: 10px; padding-right: 10px">{}</td></tr>""".format(
+                    trstyle, conn.source, conn.sink), file=f)
+        print('</table>', file=f)
+
+
+colours = {"a": "#88FFFF", "b": "#FF88FF", "c": "#8888FF", "d": "#FFFF88", "m": "#FFBBBB", "e": "#BBBBFF",
+           "w": "#FFFFBB", "n": "#BBFFFF", "s": "#FFBBBB"}
+
+
+def get_bit_info(frame, bit):
+    if (frame, bit) in bitmap:
+        group = bitmap[(frame, bit)]
+        if group.startswith("mux"):
+            label = group.split("_")[-1][0]
+            if label == "J":
+                label = group.split("_")[-1][1]
+            if label in colours:
+                colour = colours[label]
+            else:
+                colour = "#FF8888"
+        elif group.startswith("enum") or group.startswith("word"):
+            label = re.split("[_.]", group)[-1][0]
+            colour = "#88FF88"
+        else:
+            label = "?"
+            colour = "#ffffff"
+        return group, label, colour
+    else:
+        return "unknown", "&nbsp;", "#FFFFFF"
+
+
+def bit_grid_html(tileinfo, f):
+    print("<table style='font-size: 8pt; border: 2px solid black; text-align: center'>", file=f)
+    for bit in range(tileinfo.bits_per_frame):
+        print("<tr style='height: 20px'>", file=f)
+        for frame in range(tileinfo.num_frames):
+            group, label, colour = get_bit_info(frame, bit)
+            print("<td style='height: 100%; border: 2px solid black;'>", file=f)
+            title = "F{}B{}".format(frame, bit)
+            if (frame, bit) in labels:
+                for lab in sorted(labels[frame, bit]):
+                    title += "&#13;&#10;" + lab
+            print("""<a href='#{}' title='{}' style='text-decoration: none; color: #000000'>
+                    <div id='f{}b{}' style='height: 100%; background-color: {}; width: 12px' class='grp_{}'
+                    onmouseover='mov(event);' onmouseout='mou(event);'>{}</div></a></td>""".format(
+                group, title, frame, bit, colour, group, label), file=f)
+        print("</tr>", file=f)
+    print("</table>", file=f)
+
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('family', type=str,
+                    help="FPGA family (e.g. ECP5)")
+parser.add_argument('device', type=str,
+                    help="FPGA device (e.g. LFE5U-85F)")
+parser.add_argument('tile', type=str,
+                    help="Tile type (e.g. PLC2)")
+parser.add_argument('outfile', type=argparse.FileType('w'),
+                    help="output HTML file")
+
+
+def main(argv):
+    global bitmap, labels
+    bitmap = dict(dict())
+    labels = dict(dict())
+    args = parser.parse_args(argv[1:])
+    f = args.outfile
+    print(
+        """<html>
+            <head><title>{} Bit Data</title>
+        """.format(args.tile), file=f)
+    print("""
+            <script type="text/javascript">
+            origClr = {};
+            origClass = "";
+            
+            function mov(event) {
+                if (event.target.className != "unknown") {
+                    origClass = event.target.className;
+                    var elems = document.getElementsByClassName(origClass);
+                    for(var i = 0; i < elems.length; i++) {
+                       if(!(elems[i].id in origClr)) {
+                          origClr[elems[i].id] = elems[i].style.backgroundColor;
+                       }
+                       elems[i].style.backgroundColor = "white";
+                    }
+
+                }
+            }
+            
+            function mou(event) {
+                var elems = document.getElementsByClassName(origClass);
+                for(var i = 0; i < elems.length; i++) {
+                   elems[i].style.backgroundColor = origClr[elems[i].id] || "#ffffff";
+                }
+            }
+            </script>
+            </head>
+            <body>
+        """, file=f)
+    print("""<h1>{} Bit Data</h1>
+    """.format(args.tile), file=f)
+    pytang.load_database(database.get_db_root())
+    tdb = pytang.get_tile_bitdata(
+        pytang.TileLocator(args.family, args.device, args.tile))
+    ch = pytang.Chip(args.device)
+    ti = ch.get_tiles_by_type(args.tile)[0].info
+    find_bits(tdb)
+    bit_grid_html(ti, f)
+    muxes_html(tdb, f)
+    setwords_html(tdb, f)
+    setenums_html(tdb, f)
+    fixed_conns_html(tdb, f)
+    print("""</body></html>""", file=f)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
